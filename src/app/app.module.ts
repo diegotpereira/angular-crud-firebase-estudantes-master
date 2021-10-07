2427c4f83e6ee578bc7c94e0e2459205829459c1
Diego <diegoteixeirapereira@hotmail.com>
2021-10-07 12:19:07 -0300

    alteração no arquivo componente index.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Importando forms module
+import { FormsModule } from '@angular/forms'
+
+// Configurações Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+import { EstudanteComponent } from './estudantes/estudante/estudante.component';
+import { EstudantesComponent } from './estudantes/estudantes.component';
+import { EstudanteListaComponent } from './estudantes/estudante-lista/estudante-lista.component';
+import { EstudanteService } from './shared/estudante.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstudanteComponent,
+    EstudantesComponent,
+    EstudanteListaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AppRoutingModule,
+    FormsModule
+    
   ],
-  providers: [],
+  providers: [EstudanteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,17 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDogrE49NV3trk_lItcvx0cDxclo65WDes",
+    authDomain: "website-ecom-ff223.firebaseapp.com",
+    projectId: "website-ecom-ff223",
+    storageBucket: "website-ecom-ff223.appspot.com",
+    messagingSenderId: "144186283636",
+    appId: "1:144186283636:web:b111071bd8915ffa9f6321"
+  }
 };
 
 /*
